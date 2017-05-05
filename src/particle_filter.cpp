f616063472a81ab5e36828ea217150b038cb6cdb
Daniel Stang <danielstang@Daniels-MacBook-Pro.local>
2017-05-05 17:32:06 -0400

    Add update weight except for landmark matching.

--- src/particle_filter.cpp
+++ src/particle_filter.cpp
@@ -64,7 +64,6 @@ void ParticleFilter::dataAssociation(std::vector<LandmarkObs> predicted, std::ve
 	// NOTE: this method will NOT be called by the grading code. But you will probably find it useful to
 	//   implement this method and use it as a helper during the updateWeights phase.
 
-
 }
 
 void ParticleFilter::updateWeights(double sensor_range, double std_landmark[],
@@ -80,6 +79,89 @@ void ParticleFilter::updateWeights(double sensor_range, double std_landmark[],
 	//   3.33. Note that you'll need to switch the minus sign in that equation to a plus to account
 	//   for the fact that the map's y-axis actually points downwards.)
 	//   http://planning.cs.uiuc.edu/node99.html
+
+
+
+  for (int i = 0; i < particles.size(); i++) {
+    // Transform vehcile observations to current particles location with map
+    // coordinates. Set id to zero since no match has been given yet to map
+    // landmark.
+    std::vector<LandmarkObs> transformed_observations = observations;
+
+    for (int j = 0; j < observations.size(); j++) {
+      transformed_observations[j].id = 0;
+      transformed_observations[j].x = observations[j].x*cos(particles[i].theta) +
+                                      observations[j].y*sin(particles[i].theta) +
+                                      particles[i].x;
+      transformed_observations[j].y = observations[j].x*sin(particles[i].theta) +
+                                      observations[j].y*cos(particles[i].theta) +
+                                      particles[i].y;
+    }
+
+    // For each observation find the closest map landmark and set as its id.
+    // To reduce number of map landmarks that need to be searched, only search those
+    // map landmarks within sensor_range + some error.
+    std::vector<LandmarkObs> landmarks_in_sensor_range;
+
+    for (int j = 0; j < map_landmarks.landmark_list.size(); j++) {
+      float xf = map_landmarks.landmark_list[j].x_f;
+      float yf = map_landmarks.landmark_list[j].y_f;
+      int id_i = map_landmarks.landmark_list[j].id_i;
+      double sensor = sensor_range*1.1; // Add 10% for sensor error.
+
+
+      if ( (std::abs(particles[i].x - xf) < sensor) && (std::abs(particles[i].y - yf) < sensor) ) {
+        LandmarkObs templandmark;
+        templandmark.x = xf;
+        templandmark.y = yf;
+        templandmark.id = id_i;
+
+        landmarks_in_sensor_range.push_back(templandmark);
+      }
+    }
+    // Match vehicles observations transformed to map coordinates, with all the
+    // landmarks within sensor range of the particle.
+    dataAssociation(landmarks_in_sensor_range, transformed_observations);
+
+    // Update weights for each particle. Weights are the product of all differences
+    // between vehicle measurement and map landmark for each particle with some
+    // added math.
+
+    if (landmarks_in_sensor_range.size() == 0) {
+      if (observations.size() != 0) {   // If there are no landmarks in sensor range
+        particles[i].weight = 0;        // and there are observations, set weight
+      }                                 // to zero.
+      // If observations == 0 then weight will remain unchanged.
+    } else {
+      double total_weight;
+      double sigma_xx = pow(std_landmark[0], 2);
+      double sigma_yy = pow(std_landmark[1], 2);
+      for (int j = 0; j < transformed_observations.size(); j++) {
+        // Calculate difference between matched vehicle measurement and map landmark.
+        double diff_x = transformed_observations[j].x -
+                        landmarks_in_sensor_range[transformed_observations[j].id].x;
+        double diff_y = transformed_observations[j].y -
+                        landmarks_in_sensor_range[transformed_observations[j].id].y;
+
+        // Calculate numerator and denominator of weight fuction.
+        double numerator = exp(-0.5 * (pow(diff_x, 2) * pow(sigma_xx, -1) +
+                           pow(diff_y, 2) * pow(sigma_yy, -1)) );
+        double denominator = sqrt(2 * M_PI * sigma_xx * sigma_yy);
+
+        double current_weight = numerator / denominator;
+
+        if (j == 0) {  // First weight, iniialize total weight.
+          total_weight = current_weight;
+        } else {  // Total weight of particle is sum of weight for each measurement.
+          total_weight *= current_weight;
+        }
+      }
+      // Update particles weight.
+      particles[i].weight = total_weight;
+    } // NOTE: This solution does not update weights when there are no observed
+      // measurements but there are landmarks within sensor range of a particle.
+
+  }
 }
 
 void ParticleFilter::resample() {
