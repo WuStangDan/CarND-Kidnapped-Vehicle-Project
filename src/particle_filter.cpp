2b3695263fca89e0f5e9bf60fc6d5201c6d36cff
Daniel Stang <dstang14@gmail.com>
2017-05-05 20:01:33 -0400

    Add particle resampling.

--- src/particle_filter.cpp
+++ src/particle_filter.cpp
@@ -59,10 +59,29 @@ void ParticleFilter::prediction(double delta_t, double std_pos[], double velocit
 }
 
 void ParticleFilter::dataAssociation(std::vector<LandmarkObs> predicted, std::vector<LandmarkObs>& observations) {
-	// TODO: Find the predicted measurement that is closest to each observed measurement and assign the
-	//   observed measurement to this particular landmark.
-	// NOTE: this method will NOT be called by the grading code. But you will probably find it useful to
-	//   implement this method and use it as a helper during the updateWeights phase.
+	// Find the predicted measurement that is closest to each observed measurement
+	// and assign the observed measurement to this particular landmark.
+
+  for (int i = 0; i < observations.size(); i++) {
+    double min_squared_dist;
+    for (int j = 0; j < predicted.size(); j++) {
+      double distance_x = observations[i].x - predicted[j].x;
+      double distance_y = observations[i].y - predicted[j].y;
+
+      double squared_dist = pow(distance_x,2) + pow(distance_y, 2);
+
+      if (j == 0) { // On first run intialize min distance.
+        min_squared_dist = squared_dist;
+        observations[i].id = j;
+      } else {
+        if (min_squared_dist > squared_dist) {
+          min_squared_dist = squared_dist;
+          observations[i].id = j; // j or indices for predicted (landmarks_in_sensor_range)
+        }                         // will be used to call landmark closest to observation.
+      }
+    }
+
+  }
 
 }
 
@@ -93,7 +112,9 @@ void ParticleFilter::updateWeights(double sensor_range, double std_landmark[],
     for (int j = 0; j < map_landmarks.landmark_list.size(); j++) {
       float xf = map_landmarks.landmark_list[j].x_f;
       float yf = map_landmarks.landmark_list[j].y_f;
-      int id_i = map_landmarks.landmark_list[j].id_i;
+      int id_i = 0; // landmarks_in_sensor_range won't need map id since it will be
+                    // referenced directly when difference between observations
+                    // needs to be calculated.
       double sensor = sensor_range*1.1; // Add 10% for sensor error.
 
 
@@ -107,9 +128,9 @@ void ParticleFilter::updateWeights(double sensor_range, double std_landmark[],
       }
     }
     // Match vehicles observations transformed to map coordinates, with all the
-    // landmarks within sensor range of the particle.
+    // landmarks within sensor range of the particle. Indicate match by
+    // setting transformed_observations.id to matching index of landmarks_in_sensor_range.
     dataAssociation(landmarks_in_sensor_range, transformed_observations);
-
     // Update weights for each particle. Weights are the product of all differences
     // between vehicle measurement and map landmark for each particle with some
     // added math.
@@ -118,7 +139,6 @@ void ParticleFilter::updateWeights(double sensor_range, double std_landmark[],
       if (observations.size() != 0) {   // If there are no landmarks in sensor range
         particles[i].weight = 0;        // and there are observations, set weight
       }                                 // to zero.
-      // If observations == 0 then weight will remain unchanged.
     } else {
       double total_weight;
       double sigma_xx = pow(std_landmark[0], 2);
@@ -142,9 +162,13 @@ void ParticleFilter::updateWeights(double sensor_range, double std_landmark[],
         } else {  // Total weight of particle is sum of weight for each measurement.
           total_weight *= current_weight;
         }
+        //std::cout << "Weight product is " << total_weight << std::endl;
       }
       // Update particles weight.
       particles[i].weight = total_weight;
+      std::cout << i << " particles weight is now " << total_weight << std::endl;
+      std::cout << i << " particles x y theta is " << particles[i].x << " " << particles[i].y
+      << " " << particles[i].theta << std::endl;
     } // NOTE: This solution does not update weights when there are no observed
       // measurements but there are landmarks within sensor range of a particle.
 
@@ -155,7 +179,38 @@ void ParticleFilter::resample() {
 	// TODO: Resample particles with replacement with probability proportional to their weight.
 	// NOTE: You may find std::discrete_distribution helpful here.
 	//   http://en.cppreference.com/w/cpp/numeric/random/discrete_distribution
+  double sum_weight = 0;
+  std::vector<double> particle_weights; // Vector required fro discrete distribution.
+  std::vector<Particle> particles_resampled; // particles that will be resampled will go here.
 
+  for (int i = 0; i < particles.size(); i++) {
+    sum_weight += particles[i].weight;
+    particle_weights.push_back(particles[i].weight);
+  }
+
+  // Normalize all weights between 1 and 0.
+  for (int i = 0; i < particles.size(); i++) {
+    particle_weights[i] /= sum_weight;
+  }
+
+  std::default_random_engine gen;
+  // Discrete distribution will allow a distribution to be created based on
+  // particle weights and will output corresponding indices.
+  std::discrete_distribution<int> discrete_dist (particle_weights.begin(),
+                                                  particle_weights.end());
+
+  // Resample particles based on previous weights.
+  // Higher the weights, the more indentical copies of that particle
+  // will be used in the next step.
+  for (int i = 0; i < particles.size(); i++) {
+    int particle_index = discrete_dist(gen);
+    particles_resampled.push_back(particles[particle_index]);
+  }
+
+  std::cout << "Started with " << particles.size() << " and resamped to "
+  << particles_resampled.size() << std::endl;
+
+  particles = particles_resampled;
 }
 
 void ParticleFilter::write(std::string filename) {
